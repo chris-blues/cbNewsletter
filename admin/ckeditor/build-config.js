77eb7641cf778892aa603c39fdb65c2f7b41f19f
chris_blues <chris@musicchris.de>
2019-10-14 14:19:13 +0200

    still on to the file upload

--- admin/ckeditor/build-config.js
+++ admin/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) https://ckeditor.com/cke4/builder/0f706b379ef257fda72bd7b5d42b7c27
+ * (2) https://ckeditor.com/cke4/builder/8f0fafbd8d06b15cfc35dd021e0fa360
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) https://ckeditor.com/cke4/builder/download/0f706b379ef257fda72bd7b5d42b7c27
+ * (3) https://ckeditor.com/cke4/builder/download/8f0fafbd8d06b15cfc35dd021e0fa360
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -62,16 +62,12 @@ var CKBUILDER_CONFIG = {
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
-		'filebrowser' : 1,
+		'filetools' : 1,
 		'floatingspace' : 1,
 		'font' : 1,
 		'format' : 1,
 		'horizontalrule' : 1,
 		'htmlwriter' : 1,
-		'imagebase' : 1,
-		'imagepaste' : 1,
-		'imageresize' : 1,
-		'imagerotate' : 1,
 		'indentlist' : 1,
 		'link' : 1,
 		'list' : 1,
@@ -92,8 +88,9 @@ var CKBUILDER_CONFIG = {
 		'table' : 1,
 		'tableselection' : 1,
 		'tabletools' : 1,
-		'toolbar' : 1,
 		'undo' : 1,
+		'uploadfile' : 1,
+		'uploadimage' : 1,
 		'wsc' : 1
 	},
 	languages : {
