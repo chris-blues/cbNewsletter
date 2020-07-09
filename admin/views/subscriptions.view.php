25d965bf3d5d03fb2a0cf007d428f7ff78309ba2
chris_blues <chris@musicchris.de>
2020-07-09 16:54:58 +0200

    add option to order subscribers by date

--- admin/views/subscriptions.view.php
+++ admin/views/subscriptions.view.php
@@ -34,27 +34,33 @@
 
       <tr>
         <th>
-          <a
-            href="index.php<?php echo assembleGetString("string", array("view" => "subscriptions", "order" => "id", "search" => $search)); ?>"
-            class="<?php if ($order == "id") echo "active"; ?>">
-            <?php echo gettext("ID"); ?>
-	  </a>
+            <a
+                href="index.php<?php echo assembleGetString("string", array("view" => "subscriptions", "order" => "id", "search" => $search)); ?>"
+                class="<?php if ($order == "id") echo "active"; ?>">
+                <?php echo gettext("ID"); ?>
+            </a>
         </th>
         <th>
-          <a
-            href="index.php<?php echo assembleGetString("string", array("view" => "subscriptions", "order" => "name", "search" => $search)); ?>"
-            class="<?php if ($order == "name") echo "active"; ?>">
-            <?php echo gettext("Name"); ?>
-	  </a>
+            <a
+                href="index.php<?php echo assembleGetString("string", array("view" => "subscriptions", "order" => "name", "search" => $search)); ?>"
+                class="<?php if ($order == "name") echo "active"; ?>">
+                <?php echo gettext("Name"); ?>
+            </a>
         </th>
         <th>
-          <a
-            href="index.php<?php echo assembleGetString("string", array("view" => "subscriptions", "order" => "email", "search" => $search)); ?>"
-            class="<?php if ($order == "email") echo "active"; ?>">
-            <?php echo gettext("Email"); ?>
-	  </a>
+            <a
+                href="index.php<?php echo assembleGetString("string", array("view" => "subscriptions", "order" => "email", "search" => $search)); ?>"
+                class="<?php if ($order == "email") echo "active"; ?>">
+                <?php echo gettext("Email"); ?>
+            </a>
         </th>
-        <th><?php echo gettext("Subscribed"); ?></th>
+        <th>
+            <a
+                href="index.php<?php echo assembleGetString("string", array("view" => "subscriptions", "order" => "subscribed", "search" => $search)); ?>"
+                class="<?php if ($order == "subscribed") echo "active"; ?>">
+                <?php echo gettext("Subscribed"); ?>
+            </a>
+        </th>
         <th><?php echo gettext("Verified"); ?></th>
         <th><?php echo gettext("Delete"); ?></th>
       </tr>
